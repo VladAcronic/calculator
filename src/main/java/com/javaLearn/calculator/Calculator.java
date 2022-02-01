48d0d23afa95d90dc8358784e942f3ba4a13fd59
VladAcronic <drichkovvlad@gmail.com>
2022-02-01 16:36:45 +0700

    simple calculator without Exeptions

--- src/main/java/com/javaLearn/calculator/Calculator.java
+++ src/main/java/com/javaLearn/calculator/Calculator.java
@@ -1,8 +1,32 @@
 package com.javaLearn.calculator;
 
+import java.util.Optional;
+
 public class Calculator {
 
     public int add (int first, int second) {
         return first + second;
+    }
+
+    public Double divide(double first, double second) {
+        //Double result =  Double.valueOf(first/second);
+        Double result = null;
+        //double result = 0;
+
+        try {
+            //result = Double.valueOf(first / second);
+            result = (first / second);
+            if (result == Double.POSITIVE_INFINITY){
+                System.out.println(result);
+                throw new ArithmeticException("ERROR: found divide by zero");
+            }
+        }
+
+        catch (ArithmeticException ex) {
+            //System.out.println("ERROR: found divide by zero");
+            ex.printStackTrace();
+        }
+
+        return result;
     }
 }
--- src/test/java/com/javaLearn/calculator/CalculatorTest.java
+++ src/test/java/com/javaLearn/calculator/CalculatorTest.java
@@ -3,6 +3,8 @@ package com.javaLearn.calculator;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Optional;
+
 public class CalculatorTest {
 
     @Test
@@ -28,4 +30,43 @@ public class CalculatorTest {
         //then
         Assert.assertEquals(-3,result);
     }
+
+    @Test
+    public void testDivideShouldDivideWhenNumbersPositive(){
+        //given
+        Calculator calculator = new Calculator();
+
+        //when
+        double result = calculator.divide (1, 2);
+
+        //then
+        Assert.assertEquals(0.5,result,0.1);
+    }
+
+    @Test
+    public void testDivideShouldDivideWhenNumbersNegative(){
+        //given
+        Calculator calculator = new Calculator();
+
+        //when
+        Double result = calculator.divide (-2, -4);
+
+        //then
+
+        Assert.assertEquals(0.5,result, 0.001);
+    }
+
+    @Test
+    public void testDivideShouldDivideWhenDenominatorIsZero(){
+        //given
+        Calculator calculator = new Calculator();
+
+        //when
+        Double result = calculator.divide (2.0, 0);
+
+        //then
+
+        Assert.assertEquals(Double.POSITIVE_INFINITY,result,0.1);
+    }
+
 }

3ccacd15f1f95b4d6e7519f31fddec9908a4bd80
VladAcronic <drichkovvlad@gmail.com>
2022-02-01 17:15:16 +0700

    simple calculator without Exceptions

--- src/main/java/com/javaLearn/calculator/Calculator.java
+++ src/main/java/com/javaLearn/calculator/Calculator.java
@@ -1,32 +1,20 @@
 package com.javaLearn.calculator;
 
-import java.util.Optional;
-
 public class Calculator {
 
-    public int add (int first, int second) {
+    public double add (double first, double second) {
         return first + second;
     }
 
-    public Double divide(double first, double second) {
-        //Double result =  Double.valueOf(first/second);
-        Double result = null;
-        //double result = 0;
+    public double subtract (double first, double second) {
+        return first - second;
+    }
 
-        try {
-            //result = Double.valueOf(first / second);
-            result = (first / second);
-            if (result == Double.POSITIVE_INFINITY){
-                System.out.println(result);
-                throw new ArithmeticException("ERROR: found divide by zero");
-            }
-        }
+    public double multiply (double first, double second) {
+        return first * second;
+    }
 
-        catch (ArithmeticException ex) {
-            //System.out.println("ERROR: found divide by zero");
-            ex.printStackTrace();
-        }
-
-        return result;
+    public double divide(double first, double second) {
+        return first / second;
     }
 }
--- src/test/java/com/javaLearn/calculator/CalculatorTest.java
+++ src/test/java/com/javaLearn/calculator/CalculatorTest.java
@@ -3,9 +3,9 @@ package com.javaLearn.calculator;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.Optional;
-
 public class CalculatorTest {
+
+    private final double DELTA_PRECISION = 0.0001;
 
     @Test
     public void testAddShouldAddWhenNumbersPositive(){
@@ -13,10 +13,10 @@ public class CalculatorTest {
         Calculator calculator = new Calculator();
 
         //when
-        int result = calculator.add (1, 2);
+        double result = calculator.add (1.1, 2.2);
 
         //then
-        Assert.assertEquals(3,result);
+        Assert.assertEquals(3.3,result, DELTA_PRECISION);
     }
 
     @Test
@@ -25,10 +25,58 @@ public class CalculatorTest {
         Calculator calculator = new Calculator();
 
         //when
-        int result = calculator.add (-1, -2);
+        double result = calculator.add (-1.1, -2.2);
 
         //then
-        Assert.assertEquals(-3,result);
+        Assert.assertEquals(-3.3,result, DELTA_PRECISION);
+    }
+
+    @Test
+    public void testSubtractShouldSubtractWhenNumbersPositive(){
+        //given
+        Calculator calculator = new Calculator();
+
+        //when
+        double result = calculator.subtract (2.2, 1.1);
+
+        //then
+        Assert.assertEquals(1.1,result, DELTA_PRECISION);
+    }
+
+    @Test
+    public void testSubtractShouldSubtractWhenNumbersNegative(){
+        //given
+        Calculator calculator = new Calculator();
+
+        //when
+        double result = calculator.subtract (-2.2, -1.1);
+
+        //then
+        Assert.assertEquals(-1.1,result, DELTA_PRECISION);
+    }
+
+    @Test
+    public void testMultiplyShouldMultiplyWhenNumbersPositive(){
+        //given
+        Calculator calculator = new Calculator();
+
+        //when
+        double result = calculator.multiply (5.0, 1.1);
+
+        //then
+        Assert.assertEquals(5.5,result, DELTA_PRECISION);
+    }
+
+    @Test
+    public void testMultiplyShouldMultiplyWhenNumbersNegative(){
+        //given
+        Calculator calculator = new Calculator();
+
+        //when
+        double result = calculator.multiply (-5.0, 1.1);
+
+        //then
+        Assert.assertEquals(-5.5,result, DELTA_PRECISION);
     }
 
     @Test
@@ -37,10 +85,10 @@ public class CalculatorTest {
         Calculator calculator = new Calculator();
 
         //when
-        double result = calculator.divide (1, 2);
+        double result = calculator.divide (1.0, 2);
 
         //then
-        Assert.assertEquals(0.5,result,0.1);
+        Assert.assertEquals(0.5,result,DELTA_PRECISION);
     }
 
     @Test
@@ -49,24 +97,9 @@ public class CalculatorTest {
         Calculator calculator = new Calculator();
 
         //when
-        Double result = calculator.divide (-2, -4);
+        Double result = calculator.divide (-2.0, -4);
 
         //then
-
-        Assert.assertEquals(0.5,result, 0.001);
+        Assert.assertEquals(0.5,result, DELTA_PRECISION);
     }
-
-    @Test
-    public void testDivideShouldDivideWhenDenominatorIsZero(){
-        //given
-        Calculator calculator = new Calculator();
-
-        //when
-        Double result = calculator.divide (2.0, 0);
-
-        //then
-
-        Assert.assertEquals(Double.POSITIVE_INFINITY,result,0.1);
-    }
-
 }
